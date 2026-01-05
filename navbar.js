4a840b66c345286df9b95a368ff9cf60c9c7dd2f
PietroGaldi <peppe112001@gmail.com>
2026-01-05 16:46:45 +0100

    Creation of home and navbar (beta)

---
+++ navbar.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarHTML = `
+    <nav class="sidebar" id="mySidebar">
+        <div class="sidebar_header" id="toggleHeader">
+            <img src="imgs/glogo.svg" alt="group_logo" class="group_logo">
+        </div>
+        
+        <div class="nav-links-container">
+            <a href="home.html" class="nav-item"><i class="fa-solid fa-house"></i> Home</a>
+            <a href="#" class="nav-item"><i class="fa-solid fa-chart-line"></i> Page A</a>
+            <a href="#" class="nav-item"><i class="fa-solid fa-users"></i> Page B</a>
+            <a href="#" class="nav-item"><i class="fa-solid fa-circle-info"></i> Page C</a>
+        </div>
+    </nav>`;
+
+    document.querySelector(".main_area").insertAdjacentHTML("afterbegin", navbarHTML);
+
+    const currentPath = window.location.pathname.split("/").pop();
+    const navLinks = document.querySelectorAll(".nav-item");
+    
+    navLinks.forEach(link => {
+        const linkPath = link.getAttribute("href");
+        if (linkPath === currentPath || (currentPath === "" && linkPath === "home.html")) {
+            link.style.backgroundColor = "#34495e";
+            link.style.color = "#ffffff";
+        }
+    });
+
+    const header = document.getElementById("toggleHeader");
+    const sidebar = document.getElementById("mySidebar");
+
+    header.addEventListener("click", function() {
+        sidebar.classList.toggle("collapsed");
+    });
+
+});
